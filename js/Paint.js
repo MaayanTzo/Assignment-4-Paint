d42049452dc4b861635088d10431ed5e1681a4e7
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-05-30 12:57:45 +0200

    added HTML layout, got dot to print where we click functionality

--- js/Paint.js
+++ js/Paint.js
@@ -1,1 +1,50 @@
-console.log("hello");
+var b = 0;
+
+function getClickCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coor = {
+        "x": x,
+        "y": y
+    };
+    console.log(coor);
+    drawCircle(coor)
+    return coor;
+}
+
+function drawCircle(coords) {
+   // drawTheCircle = setInterval(function () {
+        var newElement = document.createElement("span");
+        newElement.className = "circle";
+        newElement.style.position = "absolute";
+        newElement.style.left = coords.x + "px";
+        newElement.style.top = coords.y + "px";
+        var par = document.getElementById("parent");
+        par.appendChild(newElement);
+       // b += 1;
+   // }, 10)
+}
+
+/*function keepDrawing() {
+    drawTheCircle=setInterval(drawCircle, 10);
+}*/
+
+/*function stopDrawCircle() {
+    clearInterval(drawTheCircle);
+} */
+
+document.getElementById("canvas").addEventListener("mousedown", function (event) {
+    getClickCoords(event)
+});
+
+//document.getElementById("canvas").addEventListener("mousedown", keepDrawing)
+
+
+
+
+/*
+
+window.addEventListener("mousedown", getClickCoords);
+window.addEventListener("mouseup", stopDrawCircle);
+
+*/
