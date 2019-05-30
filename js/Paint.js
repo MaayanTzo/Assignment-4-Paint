c3b129cc19cd6b63374cc94b8276e34222b2df07
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-05-30 14:05:11 +0200

    drawing properly but need to set mouse down and mouse up

--- js/Paint.js
+++ js/Paint.js
@@ -1,4 +1,3 @@
-var b = 0;
 
 function getClickCoords(event) {
     var x = event.clientX;
@@ -8,43 +7,22 @@ function getClickCoords(event) {
         "y": y
     };
     console.log(coor);
-    drawCircle(coor)
-    return coor;
+    drawCircle(coor);
 }
 
 function drawCircle(coords) {
-   // drawTheCircle = setInterval(function () {
         var newElement = document.createElement("span");
         newElement.className = "circle";
         newElement.style.position = "absolute";
         newElement.style.left = coords.x + "px";
         newElement.style.top = coords.y + "px";
-        var par = document.getElementById("parent");
+        var par = document.getElementById("canvas");
         par.appendChild(newElement);
-       // b += 1;
-   // }, 10)
 }
 
-/*function keepDrawing() {
-    drawTheCircle=setInterval(drawCircle, 10);
-}*/
 
-/*function stopDrawCircle() {
-    clearInterval(drawTheCircle);
-} */
 
-document.getElementById("canvas").addEventListener("mousedown", function (event) {
+document.getElementById("canvas").addEventListener("mousemove", function (event) {
     getClickCoords(event)
 });
 
-//document.getElementById("canvas").addEventListener("mousedown", keepDrawing)
-
-
-
-
-/*
-
-window.addEventListener("mousedown", getClickCoords);
-window.addEventListener("mouseup", stopDrawCircle);
-
-*/
